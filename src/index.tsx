8b1f1c3e0a7440fb4a3b004fc53e4a6f51becf98
ruritoBlogger <ruriro0125@gmail.com>
2021-05-03 02:49:46 +0900

    feat: 製作中のファイルを切り分けた

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import Main from './main';
-import './index.styl';
+import Main from "./main";
+// import Tmp from "./tmp";
+import "./index.styl";
 
 const App: React.FC = () => <Main />;
+// const App: React.FC = () => <Tmp />;
 
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,11 @@
 import styles from "./main.styl";
 
+import Intro from "./pages/intro";
+
 const Main: React.FC = () => {
   return (
     <div>
-      <h1 className={styles.title}>何見てんだ</h1>
-      <h1 className={styles.title}>まだ製作中だぞ</h1>
-      <h1 className={styles.subtitle}>
-        制作の進捗は->
-        <a href="https://twitter.com/ruriro0125">Twitter</a>
-      </h1>
-      <h1 className={styles.subtitle}>
-        ソースコードは->
-        <a href="https://github.com/ruritoBlogger/ruritoBlogger.github.io">
-          github
-        </a>
-        <h1 className={styles.subtitle}>GW中には完成させたい</h1>
-      </h1>
+      <Intro />
     </div>
   );
 };
