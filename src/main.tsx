5913601961d0ba78fd514eb4a944b6ef3775f5b8
ruritoBlogger <rurito.blog@gmail.com>
2021-05-02 14:33:46 +0900

    feat: styleを整えた

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import styles from './main.styl';
+
+const Main: React.FC = () => {
+
+    return (
+        <div>
+            <h1 className={styles.tmp}>何見てんだ</h1>
+            <h2 className={styles.tmp}>まだ製作中だぞ</h2>
+        </div>
+    )
+}
+
+export default Main;
