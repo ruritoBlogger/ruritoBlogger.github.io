c8d402a8229ccab42145a828e0fd89ba158e383e
ruritoBlogger <ruriro0125@gmail.com>
2021-05-02 15:12:06 +0900

    feat: カスタムドメインを公開した際にアクセスされても製作中だと伝わるようにページを改修

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import styles from "./main.styl";
 const Main: React.FC = () => {
   return (
     <div>
-      <h1 className={styles.tmp}>何見てんだ</h1>
-      <h1 className={styles.tmp}>まだ製作中だぞ</h1>
+      <h1 className={styles.title}>何見てんだ</h1>
+      <h1 className={styles.title}>まだ製作中だぞ</h1>
+      <h1 className={styles.subtitle}>
+        制作の進捗は
+        <a href="https://twitter.com/ruriro0125">Twitter</a>
+      </h1>
+      <h1 className={styles.subtitle}>
+        ソースコードは
+        <a href="https://github.com/ruritoBlogger/ruritoBlogger.github.io">
+          github
+        </a>
+      </h1>
     </div>
   );
 };
