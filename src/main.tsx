3a467eae7e6dc4fe0c31bbf1f5463769352c6e28
ruritoBlogger <rurito.blog@gmail.com>
2021-05-02 14:59:20 +0900

    style: prettierの設定を修正して反映

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import styles from './main.styl';
+import styles from "./main.styl";
 
 const Main: React.FC = () => {
-
-    return (
-        <div>
-            <h1 className={styles.tmp}>何見てんだ</h1>
-            <h2 className={styles.tmp}>まだ製作中だぞ</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className={styles.tmp}>何見てんだ</h1>
+      <h1 className={styles.tmp}>まだ製作中だぞ</h1>
+    </div>
+  );
+};
 
 export default Main;
