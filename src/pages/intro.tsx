74f4b8c4f8583a2ecdacb8963e080c962493a917
ruritoBlogger <ruriro0125@gmail.com>
2021-05-04 15:56:22 +0900

    feat: サイトにアクセスされた際に表示するページを実装

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -2,8 +2,15 @@ import styles from "./intro.styl";
 
 const Intro: React.FC = () => {
   return (
-    <div>
-      <h1>トップページ</h1>
+    <div className={styles.inner}>
+      <div className={styles.headline}>
+        <div className={styles.headline_inner}>
+          <h1 className={styles.title}>rurito0125's</h1>
+          <h1 className={styles.title}>Portfolio</h1>
+          <h2 className={styles.subtitle}>よわよわエンジニア</h2>
+        </div>
+      </div>
+      <img className={styles.navigate} src="/images/expand.svg"></img>
     </div>
   );
 };
