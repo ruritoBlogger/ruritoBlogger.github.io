ff386726b451cd90b88e78d27936320dd72afa2c
ruritoBlogger <rurito.blog@gmail.com>
2021-05-23 13:41:02 +0900

    レスポンシブデザインに対応

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -8,6 +8,7 @@ interface DataProps {
   title: string;
   content: string;
   link?: string;
+  link_text?: string;
   img_link?: string;
 }
 
@@ -26,6 +27,7 @@ const TimelinePage: React.FC = () => {
         "Ruby on Rails5を用いて初めてwebアプリを作りました. 夏休みを全て使ってHTML + CSS + Ruby + Railsの勉強してました.",
       link:
         "https://github.com/ruritoBlogger/Informationization-of-refrigerator",
+      link_text: "ソースコード(Github)",
     },
     {
       date: "2019/8",
@@ -39,6 +41,7 @@ const TimelinePage: React.FC = () => {
       content:
         "機械学習を用いて宝くじ(numbers3)の当選番号を予想しようとしてました. CNNやLSTMを用いてあれこれしたのですが全く当たらなかった記憶.",
       link: "https://github.com/ruritoBlogger/get-5000000000000yen",
+      link_text: "ソースコード(Github)",
     },
     {
       date: "2020/4",
@@ -47,6 +50,7 @@ const TimelinePage: React.FC = () => {
         "Ri-oneというチームの一員としてRoboCup2020 Bordeauxの出場権を獲得しました. 大会はコロナで延期になったため出場権獲得に留まっています. 出場権を獲得するためにTDPというペーパーをチームで作成しました. 私は第3章の深層強化学習部分の実装や執筆を担当しました.",
       link:
         "https://drive.google.com/file/d/1IkTX41iIFlZFI5_5l7lpCPS0X4Gi_vn8/view?usp=sharing",
+      link_text: "執筆したTDP",
     },
     {
       date: "2020/7",
@@ -54,6 +58,7 @@ const TimelinePage: React.FC = () => {
       content:
         "React + TS + Firebaseを用いたサーバーレスなwebアプリを作りました. 中身はTODO + 時間割管理が出来る(らしい). インターンの事前課題で提出しました.",
       link: "https://github.com/ruritoBlogger/TaskManagement_app",
+      link_text: "ソースコード(Github)",
       img_link: "./images/TaskManagement.png",
     },
     {
@@ -62,6 +67,7 @@ const TimelinePage: React.FC = () => {
       content:
         "楽天の二子玉川 夏の陣2020というインターン(2週間)に参加しました. お題に沿ってアプリの案出しからプロトタイプの実装までを学生でチームを作って取り組むという内容でした. フロントエンドのリーダーとして、React + Reduxを用いたフロントの環境構築から実装までを担当しました.",
       link: "https://github.com/shortintern2020-B-frontier/TeamB",
+      link_text: "ソースコード(Github)",
       img_link: "./images/TheaTalk.png",
     },
     {
@@ -76,6 +82,7 @@ const TimelinePage: React.FC = () => {
       content:
         "私が良く観てるVtuberの非公式ファンサイトを勝手に作りました. React + Typescript + Firebaseを用いたサーバーレスなwebサイトです. Atomic Componentsの概念の導入やレスポンシブデザイン対応など色々苦労しました.",
       link: "https://github.com/ruritoBlogger/Sanbaka_1.5_fan_site",
+      link_text: "ソースコード(Github)",
       img_link: "./images/fansite.png",
     },
     {
@@ -84,6 +91,7 @@ const TimelinePage: React.FC = () => {
       content:
         "GW中暇だったので作りました. いつものReact + Typescript + Firebaseで作ってます.",
       link: "https://github.com/ruritoBlogger/ruritoBlogger.github.io",
+      link_text: "ソースコード(Github)",
       img_link: "./images/portfolio.png",
     },
   ];
