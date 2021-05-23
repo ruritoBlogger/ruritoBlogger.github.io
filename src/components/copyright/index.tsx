20803b300f73b7e821e14d507c6c109f77dd6686
ruritoBlogger <rurito.blog@gmail.com>
2021-05-23 13:58:21 +0900

    add copyright

---
+++ src/components/copyright/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./copyright.styl";
+
+const Copyright: React.FC = () => {
+  return (
+    <div className={styles.inner}>
+      <p className={styles.content}>©2021 rurito0125 All Rights Reserved.</p>
+    </div>
+  );
+};
+
+export default Copyright;
