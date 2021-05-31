7e5447d5b9e564d073a027087fcd42601798bdbb
ruritoBlogger <ruriro0125@gmail.com>
2021-06-01 03:53:52 +0900

    refactor: titleを_documentで実装しないように改修

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import "./_app.sass";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>るりとのポートフォリオ</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
