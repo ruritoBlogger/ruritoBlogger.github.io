c92eff16eb5df648842ee42015fffbedf9649a66
ruritoBlogger <rurito.blog@gmail.com>
2021-06-01 04:42:11 +0900

    feat: scroll navigatorの挙動を修正

--- src/pages/intro/index.tsx
+++ src/pages/intro/index.tsx
@@ -1,6 +1,29 @@
 import styles from "./index.module.sass";
+import { useEffect } from "react";
 
 const Intro: React.FC = () => {
+  const id = "navigator";
+
+  // FIXME: null回避のやり方が微妙よな
+  const hiddenNavigator = () => {
+    if (200 < window.scrollY) {
+      document.getElementById(id)!.style.visibility = "hidden";
+    } else {
+      document.getElementById(id)!.style.visibility = "visible";
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth / window.innerHeight < 2.25) {
+      window.addEventListener("scroll", hiddenNavigator);
+    } else document.getElementById(id)!.style.visibility = "hidden";
+    return () => {
+      if (window.innerWidth / window.innerHeight < 2.25) {
+        window.removeEventListener("scroll", hiddenNavigator);
+      }
+    };
+  }, []);
+
   return (
     <div className={styles.inner}>
       <div className={styles.headline}>
@@ -14,6 +37,7 @@ const Intro: React.FC = () => {
         className={styles.navigate}
         src="/images/expand.svg"
         alt="スクロールナビゲーター"
+        id="navigator"
       ></img>
     </div>
   );
