69e19b1a4d2dbec8ae529b4cc3a454783d18075c
ruritoBlogger <rurito.blog@gmail.com>
2021-11-01 16:42:52 +0900

    chore: add type support for sass

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
 declare module "*.css";
-declare module "*.scss";
+declare module "*.sass";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
