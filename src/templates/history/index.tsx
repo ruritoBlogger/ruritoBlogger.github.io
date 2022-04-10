f4704748fb44f4b7fa2edcb69d861a1ae83c5a5d
ruritoblogger <rurito.blog@gmail.com>
2022-04-10 18:13:43 +0900

    fix: mapしているオブジェクトにkeyを設定

--- src/templates/history/index.tsx
+++ src/templates/history/index.tsx
@@ -6,7 +6,7 @@ export const HistoryTemplate: React.FC<HistoryTemplateProps> = ({ datas }) => {
     <div className={styles.inner}>
       <h1 className={styles.title}>経歴</h1>
       {datas.map((data) => (
-        <div className={styles.content_block}>
+        <div className={styles.content_block} key={data.title}>
           <h2 className={styles.subtitle}>{data.title}</h2>
           <p className={styles.content}>{data.content}</p>
           {data.link !== undefined ? <p className={styles.content}><a href={data.link}>{data.link_text}</a></p> : ""}
--- src/templates/skill/index.tsx
+++ src/templates/skill/index.tsx
@@ -6,7 +6,7 @@ export const SkillTemplate: React.FC<SkillTemplateProps> = ({ datas }) => {
     <div className={styles.inner}>
       <h1 className={styles.title}>スキル</h1>
       {datas.map((data) => (
-        <div className={styles.content_block}>
+        <div className={styles.content_block} key={data.title}>
           <h2 className={styles.subtitle}>{data.title}</h2>
           <p className={styles.content}>
             {data.content}
