0eb46d3499b367fa08e712cf189b10f5f9b97176
ruritoBlogger <ruriro0125@gmail.com>
2022-04-10 18:01:25 +0900

    fix: データ部分の文字列の可読性を上げた

--- src/datas/history.ts
+++ src/datas/history.ts
@@ -3,19 +3,30 @@ import { IHistoryData } from "../templates/type"
 export const HistoryDatas: IHistoryData[] = [
     {
         title: "2019/8 RoboCup 日本大会出場",
-        content: "RoboCup JapanOpen 2020 Nagaoka 2DSoccer Simulation部門にRi- oneというチームの一員として参加しました.",
+        content: `
+          RoboCup JapanOpen 2020 Nagaoka 2DSoccer Simulation部門に
+          Ri-oneというチームの一員として参加しました.
+        `,
         link: "#RoboCupの日本大会出場",
         link_text: "詳しい話はこちら"
     },
     {
         title: "2020/4 RoboCup 世界大会出場(権獲得)",
-        content: "RoboCup 2020 Bordeaux 2DSoccer Simulation部門にRi-oneでエントリーしました。",
+        content: `
+          RoboCup 2020 Bordeaux 2DSoccer Simulation部門に
+          Ri-oneでエントリーしました。
+        `,
         link: "#RoboCupの世界大会に出場(権獲得)",
         link_text: "詳しい話はこちら"
     },
     {
         title: "2020/8 ~ 2021/4 株式会社RUTILEA アルバイト",
-        content: "アルバイトとして開発に参加してました. C++やPythonを用いて色々やってました. 普通のPCやJetson Nanoなどの環境構築やPythonを用いたバックエンドの開発, C++やPythonを用いたハードウェアの制御スクリプトの実装などやってました.",
+        content: `
+          アルバイトとして開発に参加してました. 
+          C++やPythonを用いて色々やってました. 
+          普通のPCやJetson Nanoなどの環境構築やPythonを用いたバックエンドの開発, 
+          C++やPythonを用いたハードウェアの制御スクリプトの実装などやってました.
+        `,
     },
     {
         title: "2020/9 株式会社楽天 インターン",
@@ -31,6 +42,9 @@ export const HistoryDatas: IHistoryData[] = [
     },
     {
         title: "2021/4 ~ 株式会社HERP インターン",
-        content: "フロントエンドエンジニアとしてインターンに参加しています. Cycle.jsとかJSXとか使ってフロント書いてます.",
+        content: `
+          フロントエンドエンジニアとしてインターンに参加しています. 
+          Cycle.jsとかJSXとか使ってフロント書いてます.
+        `,
     }
 ]
