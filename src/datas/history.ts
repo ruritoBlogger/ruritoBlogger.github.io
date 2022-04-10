88c869851b1fcec14a8aa6a59ffcc8baf94013bc
ruritoBlogger <ruriro0125@gmail.com>
2022-04-10 17:01:06 +0900

    feat: Historyに乗せるデータを分割

---
+++ src/datas/history.ts
@@ -0,0 +1,36 @@
+import { IHistoryData } from "../templates/type"
+
+export const HistoryDatas: IHistoryData[] = [
+    {
+        title: "2019/8 RoboCup 日本大会出場",
+        content: "RoboCup JapanOpen 2020 Nagaoka 2DSoccer Simulation部門にRi- oneというチームの一員として参加しました.",
+        link: "#RoboCupの日本大会出場",
+        link_text: "詳しい話はこちら"
+    },
+    {
+        title: "2020/4 RoboCup 世界大会出場(権獲得)",
+        content: "RoboCup 2020 Bordeaux 2DSoccer Simulation部門にRi-oneでエントリーしました。",
+        link: "#RoboCupの世界大会に出場(権獲得)",
+        link_text: "詳しい話はこちら"
+    },
+    {
+        title: "2020/8 ~ 2021/4 株式会社RUTILEA アルバイト",
+        content: "アルバイトとして開発に参加してました. C++やPythonを用いて色々やってました. 普通のPCやJetson Nanoなどの環境構築やPythonを用いたバックエンドの開発, C++やPythonを用いたハードウェアの制御スクリプトの実装などやってました.",
+    },
+    {
+        title: "2020/9 株式会社楽天 インターン",
+        content: "株式会社楽天の「二子玉川 夏の陣 2020 」に参加してました.",
+        link: "#楽天のインターンに参加",
+        link_text: "詳しい話はこちら",
+    },
+    {
+        title: "2020/9 株式会社オプティム インターン",
+        content: "オプティム サマーインターン2020に参加してました.",
+        link: "#オプティムの夏季インターンに参加",
+        link_text: "詳しい話はこちら",
+    },
+    {
+        title: "2021/4 ~ 株式会社HERP インターン",
+        content: "フロントエンドエンジニアとしてインターンに参加しています. Cycle.jsとかJSXとか使ってフロント書いてます.",
+    }
+]
